17ed0999ad2757aef44c6cf88b22acefde75a7f2
Gregor Fabritius <fabritius@grefab.de>
2016-06-20 22:22:31 +0200

    Using make_shared where applicable.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@ int main() {
     {
         brain b;
 
-        auto n1 = b.add_neuron(neuron_ptr(new neuron(1, 0.5)));
-        auto n2 = b.add_neuron(neuron_ptr(new neuron(1, 0.5)));
-        b.add_connection(n1, connection_ptr(new connection(n2, 1, 1)));
-        b.add_connection(n2, connection_ptr(new connection(n1, 1, 1)));
+        auto n1 = b.add_neuron(std::make_shared<neuron>(1, 0.5));
+        auto n2 = b.add_neuron(std::make_shared<neuron>(1, 0.5));
+        b.add_connection(n1, std::make_shared<connection>(n2, 1, 1));
+        b.add_connection(n2, std::make_shared<connection>(n1, 1, 1));
         n1->apply_pulse(1, 0, &b);
 
         b.run();
